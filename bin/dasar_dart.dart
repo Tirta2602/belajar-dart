6838766a87f22ce23f7690414cb9130a649abf5d
syamsul0208 <if17.syamsulmaarif@mhs.ubpkarawang.ac.id>
2025-04-15 13:15:19 +0700

    belarjar variable

--- bin/dasar_dart.dart
+++ bin/dasar_dart.dart
@@ -1,5 +1,7 @@
 import 'package:dasar_dart/dasar_dart.dart' as dasar_dart;
 
-void main(List<String> arguments) {
-  print('Hello world: ${dasar_dart.calculate()}!');
+void main() {
+var alatTidur = 'kasur'; 
+print(alatTidur);
+
 }
